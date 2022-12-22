6a34738aced36d57967aa4304e9268ca8a8ec84c
Scott Foster <scottgarryfoster@googlemail.com>
2022-12-22 17:40:20 +0000

    Added test for non-existent account in intergration.

--- TestApplication/TestApplicationTests/AccountOperationsIntergrationTests.cs
+++ TestApplication/TestApplicationTests/AccountOperationsIntergrationTests.cs
@@ -29,5 +29,23 @@ namespace TestApplicationTests
             // Assert
             Assert.AreEqual(expectedBalance, actualBalance);
         }
+
+        [TestMethod]
+        public void GetBalanceForSingleAccount_Returns0_WhenRetrivingNonExistantAccountTest()
+        {
+            int accountNumber = 0;
+            int expectedBalance = 0;
+
+            // Arrange
+            IAccountData database = new AccountDatabase();
+
+            var testingClass = new AccountOperations(database);
+
+            // Act
+            int actualBalance = testingClass.GetBalanceForSingleAccount(accountNumber);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
     }
 }
