044ab1ed16ccdd194db0811dcfdf8c94b80e7f9e
Scott Foster <scottgarryfoster@googlemail.com>
2022-12-22 17:04:48 +0000

    Added Account Operations with Balance Check.

---
+++ TestApplication/TestApplication/AccountOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    public class AccountOperations
+    {
+        private IAccountData accountData;
+
+        public AccountOperations(IAccountData accountData) 
+        {
+            this.accountData = accountData ?? throw new ArgumentNullException(nameof(accountData));
+        }
+
+        public int GetBalanceForSingleAccount(int accountId) 
+        {
+            IAccount account = accountData.GetAccount(accountId);
+            return account.Balance;
+        }
+    }
+}
---
+++ TestApplication/TestApplicationTests/AccountOperationsTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplication;
+
+namespace TestApplicationTests
+{
+    [TestClass]
+    public class AccountOperationsTests
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void OnConstruction_ThrowsNullArgumentException_WhenDatabaseIsNullTest()
+        {
+            // Arrange
+            IAccountData nullDatabase = null;
+
+            // Act / Assert
+            new AccountOperations(nullDatabase);
+        }
+
+        [TestMethod]
+        public void GetBalanceForSingleAccount_ReturnsBalanceFromDatabase_WhenDatabaseProvidesTheAccountTest()
+        {
+            int accountNumber = 12345;
+            int expectedBalance = 420;
+
+            // Arrange
+            var mockAccount = new Mock<IAccount>();
+            mockAccount.Setup(x => x.Balance).Returns(expectedBalance);
+
+            var mockDatabase = new Mock<IAccountData>();
+            mockDatabase.Setup(x => x.GetAccount(accountNumber)).Returns(mockAccount.Object);
+
+            var testingClass = new AccountOperations(mockDatabase.Object);
+
+            // Act
+            int actualBalance = testingClass.GetBalanceForSingleAccount(accountNumber);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+
+        [TestMethod]
+        public void GetBalanceForSingleAccount_ReturnsZero_WhenDatabaseProvidesTheAccountTest()
+        {
+            int accountNumber = 12345;
+            int expectedBalance = 0;
+
+            // Arrange
+            var mockAccount = new Mock<IAccount>();
+
+            var mockDatabase = new Mock<IAccountData>();
+            mockDatabase.Setup(x => x.GetAccount(accountNumber)).Returns(mockAccount.Object);
+
+            var testingClass = new AccountOperations(mockDatabase.Object);
+
+            // Act
+            int actualBalance = testingClass.GetBalanceForSingleAccount(accountNumber);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+    }
+}
